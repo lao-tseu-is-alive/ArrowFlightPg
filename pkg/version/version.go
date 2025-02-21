1f94029a72eb496f1382171d09c22a52a1e8cc21
cgil <lao.tseu.is.alive@gmail.com>
2025-02-21 13:33:12 +0100

    preparing env

---
+++ pkg/version/version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	APP        = "ArrowFlightPg"
+	AppSnake   = "arrow-flight-pg"
+	VERSION    = "0.1.0"
+	REPOSITORY = "https://github.com/lao-tseu-is-alive/ArrowFlightPg"
+	REVISION   = "unknown"
+	BuildStamp = "unknown"
+)
